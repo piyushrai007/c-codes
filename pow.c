efbec81abf5944df6fa8cfb0ec39be554e4fc117
piyushrai007 <111386563+piyushrai007@users.noreply.github.com>
2022-12-08 23:37:44 +0530

    Add files via upload

---
+++ pow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(int argc, char const *argv[])
+{
+    float a;
+    float b;
+    printf("enter the value os a and b\n");
+    scanf("%f",&a);
+    scanf("%f",&b);
+     printf("the a raised to b is %f", pow(a,b));
+    return 0;
+}
+
